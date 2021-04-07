e8e92583698a4444ccbc83ebf772a96243527d76
jakeshoemaker <jakeshoe3@gmail.com>
2021-04-07 15:12:49 -0500

    updated API documentation, using swagger to document API

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,21 @@
 import express from "express";
 import dotenv from 'dotenv';
-import bodyParser from "body-parser";
-import cors from "cors";
-
-
-
 import router from './routes/router'
+import yaml from 'js-yaml'
+import fs from 'fs'
 
+//let inputYaml = 'swagger.yaml'
+//let outputJson = 'swagger.json'
+//let obj = yaml.load(fs.readFileSync(inputYaml, {encoding: 'utf-8'}))
+//fs.writeFileSync(outputJson, JSON.stringify(obj, null, 2))
+
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json')
 dotenv.config()
 const app = express()
 
-app.use([express.json(),express.urlencoded({extended:true})])//parse url and json data
+app.use([express.json(),express.urlencoded({extended:true})])
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use('/',router)
 
 
