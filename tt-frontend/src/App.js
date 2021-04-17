d12eeb521e882b6a188942faa019db5bf024f2cf
jakeshoemaker <jakeshoe3@gmail.com>
2021-04-17 12:22:20 -0500

    added react scaffolding

---
+++ tt-frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+const App = () => {
+  return (
+    <Button type="primary"> TEST </Button>
+  )
+}
+
+export default App;
